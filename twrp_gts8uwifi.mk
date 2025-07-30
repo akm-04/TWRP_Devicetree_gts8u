af77c7e8cf31fafb7aaaacad510ab3879884c05c
AKM <akmabrar04@gmail.com>
2025-07-30 16:56:02 +0600

    device: reorder `twrp_gts8uwifi.mk`and added charger_res_image
    - Reordered PRODUCT_* definitions for consistency and readability
    - Removed unused `PRIVATE_BUILD_DESC` and `BUILD_FINGERPRINT` overrides
    - Include the charger_res_images package so that the device’s charging UI
      resources are built into the final image.
    
    Signed‑off‑by: AKM Abrar <akmabrar04@gmail.com>

--- twrp_gts8uwifi.mk
+++ twrp_gts8uwifi.mk
@@ -18,19 +18,19 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from gts8uwifi device
 $(call inherit-product, device/samsung/gts8uwifi/device.mk)
 
-PRODUCT_DEVICE := gts8uwifi
-PRODUCT_NAME := twrp_gts8uwifi
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-X900
-PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Copy device-specific recovery root overlay (fstab, init scripts, etc.)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/samsung/gts8uwifi/recovery/root,recovery/root)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="gts8uwifixx-user 12 SP2A.220305.013 X900XXU9DYE5 release-keys"
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
 
-BUILD_FINGERPRINT := samsung/gts8uwifixx/gts8uwifi:12/SP2A.220305.013/X900XXU9DYE5:user/release-keys
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gts8uwifi
+PRODUCT_NAME := twrp_gts8uwifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-X900
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
